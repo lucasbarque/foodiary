ca47942b68bb261b0938e9a400a83705482ce3c4
Lucas Barque <lucasbarquedasilva@hotmail.com>
2025-07-19 11:41:32 -0400

    feat: create SignInController to manage user sign-in logic in foodiary-api

---
+++ foodiary-api/src/controllers/SignInController.ts
@@ -0,0 +1,41 @@
+import { eq } from "drizzle-orm";
+import { db } from "../db";
+import { HttpRequest, HttpResponse } from "../types/Http";
+import { badRequest, ok, unauthorized } from "../utils/http";
+import { z } from "zod";
+import { usersTable } from "../db/schema";
+import { compare, compareSync } from "bcryptjs";
+
+const schema = z.object({
+  email: z.email(),
+  password: z.string().min(8),
+});
+
+export class SignInController {
+  static async handle({ body }: HttpRequest): Promise<HttpResponse> {
+    const { success, error, data } = schema.safeParse(body);
+
+    if (!success) {
+      return badRequest({ errors: error.issues });
+    }
+
+    const user = await db.query.usersTable.findFirst({
+      columns: { id: true, email: true, password: true },
+      where: eq(usersTable.email, data.email),
+    });
+
+    if (!user) {
+      return unauthorized({ error: "Invalid credentials" });
+    }
+
+    const isPasswordValid = await compare(data.password, user.password);
+
+    if (!isPasswordValid) {
+      return unauthorized({ error: "Invalid credentials" });
+    }
+
+    return ok({
+      data,
+    });
+  }
+}
