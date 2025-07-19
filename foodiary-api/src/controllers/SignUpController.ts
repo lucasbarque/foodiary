abfced87bb035a1f09e67c20eeaab5edc5a25e12
Lucas Barque <lucasbarquedasilva@hotmail.com>
2025-07-19 11:41:36 -0400

    feat: implement SignUpController to handle user registration logic in foodiary-api

---
+++ foodiary-api/src/controllers/SignUpController.ts
@@ -0,0 +1,62 @@
+import { z } from "zod";
+import { HttpRequest, HttpResponse } from "../types/Http";
+import { badRequest, conflict, created } from "../utils/http";
+import { db } from "../db";
+import { eq } from "drizzle-orm";
+import { usersTable } from "../db/schema";
+import { hash } from "bcryptjs";
+const schema = z.object({
+  goal: z.enum(["lose", "maintain", "gain"]),
+  gender: z.enum(["male", "female"]),
+  birthDate: z.iso.date(),
+  height: z.number(),
+  weight: z.number(),
+  activityLevel: z.number().min(1).max(5),
+  account: z.object({
+    name: z.string().min(1),
+    email: z.email(),
+    password: z.string().min(8),
+  }),
+});
+
+export class SignUpController {
+  static async handle({ body }: HttpRequest): Promise<HttpResponse> {
+    const { success, error, data } = schema.safeParse(body);
+
+    if (!success) {
+      return badRequest({ errors: error.issues });
+    }
+
+    const userAlreadyExists = await db.query.usersTable.findFirst({
+      columns: { email: true },
+      where: eq(usersTable.email, data.account.email),
+    });
+
+    if (userAlreadyExists) {
+      return conflict({ error: "This email is already in use" });
+    }
+
+    const { account, ...rest } = data;
+
+    const hashedPassword = await hash(account.password, 8);
+
+    const [user] = await db
+      .insert(usersTable)
+      .values({
+        ...account,
+        ...rest,
+        calories: 0,
+        proteins: 0,
+        carbohydrates: 0,
+        fats: 0,
+        password: hashedPassword,
+      })
+      .returning({
+        id: usersTable.id,
+      });
+
+    return created({
+      userId: user.id,
+    });
+  }
+}
