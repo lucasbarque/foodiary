c3784d5b148a6cf5896b18a0099bbed6e9aaa23c
Lucas Barque <lucasbarquedasilva@hotmail.com>
2025-07-20 22:02:06 -0400

    feat: create Page component to structure private app layout with header, meals list, and bottom bar

---
+++ mobile/src/app/(private)/index.tsx
@@ -0,0 +1,14 @@
+import { View } from "react-native";
+import { HomeHeader } from "../../components/HomeHeader";
+import { MealsList } from "../../components/MealsList";
+import { CreateMealBottomBar } from "../../components/CreateMealBottomBar";
+
+export default function Page() {
+  return (
+    <View className="flex-1 bg-white">
+      <HomeHeader />
+      <MealsList />
+      <CreateMealBottomBar />
+    </View>
+  );
+}
