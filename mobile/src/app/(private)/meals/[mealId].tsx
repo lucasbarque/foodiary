28bd180f372b534e46922c0cbe6743d76864f3eb
Lucas Barque <lucasbarquedasilva@hotmail.com>
2025-07-20 22:02:10 -0400

    feat: add MealDetails component to display meal information and navigation

---
+++ mobile/src/app/(private)/meals/[mealId].tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import { router, useLocalSearchParams } from "expo-router";
+import { Button } from "../../../components/Button";
+
+export default function MealDetails() {
+  const { mealId } = useLocalSearchParams();
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Detalhes da refeição {mealId}</Text>
+
+      <Button onPress={router.back}>Voltar</Button>
+    </View>
+  );
+}
