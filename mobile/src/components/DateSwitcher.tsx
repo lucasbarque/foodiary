2ba1e6519353add78f71c065ecf1e2698879dd8a
Lucas Barque <lucasbarquedasilva@hotmail.com>
2025-07-26 13:56:26 -0400

    feat: refactor DateSwitcher component to accept currentDate and navigation callbacks, enhancing reusability and date formatting

--- mobile/src/components/DateSwitcher.tsx
+++ mobile/src/components/DateSwitcher.tsx
@@ -1,19 +1,36 @@
 import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react-native";
 import { Text, TouchableOpacity, View } from "react-native";
 import { colors } from "../styles/colors";
+import { formatDate } from "../utils/formatDate";
 
-export function DateSwitcher() {
+interface IDateSwitcherProps {
+  currentDate: Date;
+  onPreviousDate: () => void;
+  onNextDate: () => void;
+}
+
+export function DateSwitcher({
+  currentDate,
+  onPreviousDate,
+  onNextDate,
+}: IDateSwitcherProps) {
   return (
     <View className="px-2 mt-3 flex-row items-center justify-between">
-      <TouchableOpacity className="size-12 items-center justify-center">
+      <TouchableOpacity
+        className="size-12 items-center justify-center"
+        onPress={onPreviousDate}
+      >
         <ChevronLeftIcon size={20} color={colors.black[700]} />
       </TouchableOpacity>
 
       <Text className="font-sans-medium text-base text-gray-700 tracking-[1.28px]">
-        HOJE, 20 DE JULHO
+        {formatDate(currentDate)}
       </Text>
 
-      <TouchableOpacity className="size-12 items-center justify-center">
+      <TouchableOpacity
+        className="size-12 items-center justify-center"
+        onPress={onNextDate}
+      >
         <ChevronRightIcon size={20} color={colors.black[700]} />
       </TouchableOpacity>
     </View>
