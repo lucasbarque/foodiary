1179f5c0e78828705d8febe256b6a00c25c3a31c
Lucas Barque <lucasbarquedasilva@hotmail.com>
2025-07-26 15:33:59 -0400

    refactor: reorganize imports and enhance error messages in parseProtectedEvent function for improved readability and maintainability

--- foodiary-api/src/utils/parseProtectedEvent.ts
+++ foodiary-api/src/utils/parseProtectedEvent.ts
@@ -1,24 +1,23 @@
 import { APIGatewayProxyEventV2 } from "aws-lambda";
-import { HttpRequest, ProtectedHttpRequest } from "../types/Http";
-import { parseEvent } from "./parseEvent";
 import { validateAccessToken } from "../lib/jwt";
+import { ProtectedHttpRequest } from "../types/Http";
+import { parseEvent } from "./parseEvent";
 
 export function parseProtectedEvent(
   event: APIGatewayProxyEventV2
 ): ProtectedHttpRequest {
   const baseEvent = parseEvent(event);
-
   const { authorization } = event.headers;
 
   if (!authorization) {
-    throw new Error("Access token not provided");
+    throw new Error("Access token not provided.");
   }
 
   const [, token] = authorization.split(" ");
   const userId = validateAccessToken(token);
 
   if (!userId) {
-    throw new Error("Invalid access token");
+    throw new Error("Invalid access token.");
   }
 
   return {
