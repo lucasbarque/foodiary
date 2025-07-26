f4e44cd7e032b8a5ed4443c49490ea7decd0a7d3
Lucas Barque <lucasbarquedasilva@hotmail.com>
2025-07-26 13:09:47 -0400

    feat: refactor useCreateMeal hook to accept parameters for file type and success callback, enhancing flexibility and usability

--- mobile/src/hooks/useCreateMeal.ts
+++ mobile/src/hooks/useCreateMeal.ts
@@ -2,13 +2,19 @@ import { useMutation } from "@tanstack/react-query";
 import { httpClient } from "../services/httpClient";
 
 import * as FileSystem from "expo-file-system";
+import { router } from "expo-router";
 
 type CreateMealResponse = {
   uploadURL: string;
   mealId: string;
 };
 
-export function useCreateMeal(fileType: "image/jpeg" | "audio/m4a") {
+type CreateMealParams = {
+  fileType: "image/jpeg" | "audio/m4a";
+  onSuccess(mealId: string): void;
+};
+
+export function useCreateMeal({ fileType, onSuccess }: CreateMealParams) {
   const { mutateAsync, isPending } = useMutation({
     mutationFn: async (uri: string) => {
       const { data } = await httpClient.post<CreateMealResponse>("/meals", {
@@ -19,6 +25,11 @@ export function useCreateMeal(fileType: "image/jpeg" | "audio/m4a") {
         httpMethod: "PUT",
         uploadType: FileSystem.FileSystemUploadType.BINARY_CONTENT,
       });
+
+      return { mealId: data.mealId };
+    },
+    onSuccess: ({ mealId }) => {
+      onSuccess(mealId);
     },
   });
   return {
