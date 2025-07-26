0518ade98f8938bbafd8bca9cc676b82cf4f1c3b
Lucas Barque <lucasbarquedasilva@hotmail.com>
2025-07-25 23:52:35 -0400

    feat: refactor MealsList component to fetch meals from API and handle empty state

--- mobile/src/components/MealsList.tsx
+++ mobile/src/components/MealsList.tsx
@@ -4,33 +4,8 @@ import { DateSwitcher } from "./DateSwitcher";
 import { DailyStats } from "./DailyStats";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { useAuth } from "../hooks/useAuth";
-
-const meals = [
-  {
-    id: String(Math.random()),
-    name: "Café da manhã",
-  },
-  {
-    id: String(Math.random()),
-    name: "Almoço",
-  },
-  {
-    id: String(Math.random()),
-    name: "Jantar",
-  },
-  {
-    id: String(Math.random()),
-    name: "Café da manhã",
-  },
-  {
-    id: String(Math.random()),
-    name: "Almoço",
-  },
-  {
-    id: String(Math.random()),
-    name: "Jantar",
-  },
-];
+import { useQuery } from "@tanstack/react-query";
+import { httpClient } from "../services/httpClient";
 
 function MealsListHeader() {
   const { user } = useAuth();
@@ -73,8 +48,36 @@ function Separator() {
   return <View className="h-8" />;
 }
 
+type Meal = {
+  id: string;
+  name: string;
+  icon: string;
+  foods: {
+    name: string;
+    quantity: string;
+    calories: number;
+    proteins: number;
+    carbohydrates: number;
+    fats: number;
+  }[];
+  createdAt: Date;
+};
+
 export function MealsList() {
   const { bottom } = useSafeAreaInsets();
+
+  const { data: meals } = useQuery({
+    queryKey: ["meals"],
+    queryFn: async () => {
+      const { data } = await httpClient.get<{ meals: Meal[] }>("/meals", {
+        params: {
+          date: "2025-07-25",
+        },
+      });
+      console.log(data);
+      return data.meals;
+    },
+  });
 
   return (
     <View>
@@ -83,6 +86,7 @@ export function MealsList() {
         contentContainerStyle={{ paddingBottom: 80 + bottom + 16 }}
         keyExtractor={(meal) => meal.id}
         ListHeaderComponent={<MealsListHeader />}
+        ListEmptyComponent={<Text>Nenhuma refeição cadastrada...</Text>}
         ItemSeparatorComponent={Separator}
         renderItem={({ item: meal }) => (
           <View className="mx-5">
