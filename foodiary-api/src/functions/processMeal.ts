8f6b456d951a8a5b1ec4b930b73b1dba48b0f2e2
Lucas Barque <lucasbarquedasilva@hotmail.com>
2025-07-26 16:31:52 -0400

    chore: add logging and error handling in processMeal function to enhance debugging and reliability

--- foodiary-api/src/functions/processMeal.ts
+++ foodiary-api/src/functions/processMeal.ts
@@ -2,10 +2,16 @@ import { SQSEvent } from "aws-lambda";
 import { ProcessMeal } from "../queues/ProcessMeal";
 
 export async function handler(event: SQSEvent) {
-  await Promise.all(
-    event.Records.map(async (record) => {
-      const body = JSON.parse(record.body);
-      await ProcessMeal.process(body);
-    })
-  );
+  console.log(JSON.stringify(event, null, 2));
+
+  try {
+    await Promise.all(
+      event.Records.map(async (record) => {
+        const body = JSON.parse(record.body);
+        await ProcessMeal.process(body);
+      })
+    );
+  } catch (error) {
+    console.log(JSON.stringify(error, null, 2));
+  }
 }
