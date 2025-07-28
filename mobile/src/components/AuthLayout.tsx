688acc0072a868250a3de23cc08287d435fe9e43
lucasbarque <lucasbarquedasilva@hotmail.com>
2025-07-27 22:36:03 -0400

    fix: add padding and center alignment to subtitle text in AuthLayout component

--- mobile/src/components/AuthLayout.tsx
+++ mobile/src/components/AuthLayout.tsx
@@ -29,7 +29,7 @@ export function AuthLayout({
           <Text className="text-black-700 font-sans-semibold tracking-[-0.32px] text-[32px] text-center">
             {title}
           </Text>
-          <Text className="mt-1 text-gray-700 text-base font-sans-regular">
+          <Text className="mt-1 px-8 text-center text-gray-700 text-base font-sans-regular">
             {subtitle}
           </Text>
         </View>
